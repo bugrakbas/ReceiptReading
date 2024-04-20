fbf5c81046d94392e38bef34680057b73d257de2
bugrakbas <bugrahan.akbas@cci.com.tr>
2024-04-20 20:27:18 +0300

    Add project files.

---
+++ ConsoleApp1/Models/JsonReadModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class ReceiptItem
+    {
+        public string description { get; set; }
+        public BoundingPoly boundingPoly { get; set; }
+    }
+
+    public class BoundingPoly
+    {
+        public List<Vertex> vertices { get; set; }
+    }
+
+    public class Vertex
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp1.Models;
+using Newtonsoft.Json;
+public class ReceiptParser
+{
+    public void ParseJson(string jsonFilePath)
+    {
+        try
+        {
+            string jsonText = File.ReadAllText(jsonFilePath);
+
+            List<ReceiptItem> items = JsonConvert.DeserializeObject<List<ReceiptItem>>(jsonText);
+            var text = items.FirstOrDefault().description;
+            Console.WriteLine(text);
+            // If you want to see the coordinate values, you can use this code
+            //foreach (ReceiptItem item in items)
+            //{
+            //    Console.WriteLine(item.description); // Öğe açıklamasını yazdır
+
+            //    Console.WriteLine("Koordinatlar:");
+            //    foreach (Vertex vertex in item.boundingPoly.vertices)
+            //    {
+            //        Console.WriteLine($"x: {vertex.x}, y: {vertex.y}");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata: " + ex.Message);
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReceiptParser parser = new ReceiptParser();
+        parser.ParseJson("veri.json");
+    }
+}
